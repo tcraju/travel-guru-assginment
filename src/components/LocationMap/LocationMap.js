1427f894745a9e9ce3ced31209587fd385a83761
tcraju <tcnajmul@gmail.com>
2020-09-24 15:57:49 +0600

    map part roughly done

--- src/components/LocationMap/LocationMap.js
+++ src/components/LocationMap/LocationMap.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from 'react'
+import { Map, TileLayer } from 'react-leaflet'
+
+
+
+
+
+
+
+const position = [21.43973 , 92.00955]
+
 
 const LocationMap = () => {
     return (
-        <div>
-            <h2>Map</h2>
-        </div>
+
+            <Map center={position} zoom={10}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                />
+            </Map>
     );
 };
 
 export default LocationMap;
+
+
